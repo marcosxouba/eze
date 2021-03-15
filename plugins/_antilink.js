a586e802f89f4c687bb3781042298373ba17f916
Ezequiel7u7xD <74691619+Ezequiel7u7xD@users.noreply.github.com>
2021-03-15 16:55:34 -0600

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -6,7 +6,7 @@ handler.before = m => {
   let chat = global.DATABASE.data.chats[m.chat]
   let isGroupLink = linkRegex.exec(m.text)
 
-  if (chat.antiLink && isGroupLink) m.reply('/kick ${name} (@${who.replace(/@.+/')
+  if (chat.antiLink && isGroupLink) m.reply('/kick @${who.replace(/@.+/, '')}')
 
   return true
 }
