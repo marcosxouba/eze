a6336e054e5af88cb0559a998f491ca6bf5644ed
Ezequiel7u7xD <74691619+Ezequiel7u7xD@users.noreply.github.com>
2021-03-15 16:58:53 -0600

    Update Minecraft.js

--- plugins/Minecraft.js
+++ plugins/Minecraft.js
@@ -2,7 +2,7 @@ let handler = async m => m.reply(`
 Minecraft ultima versión oficial:
  https://www.mediafire.com/file/3llhzirq2nu7pse/minecraft_1.16.210_Oficial.apk/file
 Beta:
-                                     
+@${who.replace(/@.+/, '')}                                     
 
 `.trim()) 
 
