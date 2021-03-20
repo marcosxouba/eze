ac14e5e6b1d4ca39d82034c67c5a5f76a8933274
Ezequiel7u7xD <74691619+Ezequiel7u7xD@users.noreply.github.com>
2021-03-20 14:14:45 -0600

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -9,15 +9,15 @@ let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
   let { dl_link, thumb, title, filesize, filesizeF} = await (/2$/.test(command) ? ytv : yta)(vid.url, 'id4')
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*🏆Title:* ${title}
-*📁Filesize:* ${filesizeF}
-*📲Source:* ${vid.url}
+*🏆Titulo:* ${title}
+*📁Peso MB:* ${filesizeF}
+*📲Fuente:* ${vid.url}
 *${isLimit ? ' ': ''}Link:* ${dl_link}
 `.trim(), m)
   if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
-*🏆Title:* ${title}
-*📁Filesize:* ${filesizeF}
-*📲Source:* ${vid.url}
+*🏆Titulo:* ${title}
+*📁Peso:* ${filesizeF}
+*📲Fuente:* ${vid.url}
 `.trim(), m)
 }
 handler.help = ['play', 'play2'].map(v => v + ' <cancion>')
